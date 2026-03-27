3069f68d58644db042bf73e1ff991ceafd8eb413
808654-hayat <808654@manadulted.ac.uk>
2026-03-27 22:08:08 +0000

    Integrated Quote API with loading and error states

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+
 const nameHeading = document.querySelector("h1");
 
 const hour= new Date().getHours();
+
+const time = new Date().toLocaleString();
+
+
+const quoteBtn = document.getElementById('quote-btn');
+const quoteText = document.getElementById('quote-text');
+const quoteAuthor = document.getElementById('quote-author');
+
+
+
+document.getElementById("time").textContent= time
 
 if (hour<12) {
     nameHeading.textContent= "Good Morning, Hayat";
@@ -13,4 +25,19 @@ else if (hour < 18) {
 
 console.log("The current hour is:" + hour);
 
+/*const personalStatement= document.querySelector(`.highlight`);
+personalStatement.style.backgroundColor = "blue";*/
 
+document.getElementById("copyright").textContent=new Date().getFullYear()
+
+
+quoteBtn.addEventListener('click', function() {
+    quoteText.textContent = "Fetching....";
+    fetch('https://dummyjson.com/quotes/random')
+    .then(response => response.json())
+    .then(data => {
+        quoteText.textContent = `"${data.quote}"`;
+        quoteAuthor.textContent = `— ${data.author}`;
+    })
+    .catch(error => {quoteText.textContent = "Could not connect to the quote server."});   
+});
