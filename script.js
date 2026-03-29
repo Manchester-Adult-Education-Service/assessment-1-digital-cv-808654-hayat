86f5dd2762ed76b73f254619bab763d5903fb796
808654-hayat <808654@manadulted.ac.uk>
2026-03-29 21:12:41 +0000

    Advanced task:added second advice API with nested data

--- script.js
+++ script.js
@@ -28,16 +28,45 @@ console.log("The current hour is:" + hour);
 /*const personalStatement= document.querySelector(`.highlight`);
 personalStatement.style.backgroundColor = "blue";*/
 
-document.getElementById("copyright").textContent=new Date().getFullYear()
+const currentYear = new Date().getFullYear();
+const copyright = document.getElementById('copyright');
+copyright.textContent = `Copyright © ${currentYear} Hayat Bekhouche`;
+
 
 
 quoteBtn.addEventListener('click', function() {
     quoteText.textContent = "Fetching....";
+    document.body.style.cursor = "wait";
     fetch('https://dummyjson.com/quotes/random')
     .then(response => response.json())
     .then(data => {
         quoteText.textContent = `"${data.quote}"`;
         quoteAuthor.textContent = `— ${data.author}`;
+        document.body.style.cursor = "default";
     })
-    .catch(error => {quoteText.textContent = "Could not connect to the quote server."});   
+    .catch(error => {quoteText.textContent = "Could not connect to the quote server."});
+        document.body.style.cursor = "default";
+        console.error("Fetch error:", error);
 });
+
+
+const adviceText = document.getElementById("advice-text");
+const adviceBtn = document.getElementById('advice-btn');
+
+adviceBtn.addEventListener('click', function() {
+    adviceText.textContent = "Consulting the experts...";
+    fetch('https://api.adviceslip.com/advice')
+        .then(response => response.json())
+        .then(data => {
+            adviceText.textContent = data.slip.advice;
+        })
+        .catch(err => {
+            adviceText.textContent = "The oracle is busy. Try again soon.";
+        });
+});
+
+
+window.addEventListener("load", () => {
+    quoteBtn.click();
+    adviceBtn.click();
+})
